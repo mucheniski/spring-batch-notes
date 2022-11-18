6cc4ec0b59afd6f7b5738d82b7384d3151224e94
mucheniski <mucheniski@gmail.com>
2022-11-18 11:03:41 -0300

    Processador composto

--- projetos-modelos/Processors/ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorCustomConfig.java
+++ projetos-modelos/Processors/ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorCustomConfig.java
@@ -2,6 +2,8 @@ package com.springbatch.processadorvalidacao.processor;
 
 import com.springbatch.processadorvalidacao.dominio.Cliente;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
 import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidationException;
@@ -18,15 +20,29 @@ public class ProcessadorValidacaoProcessorCustomConfig {
 	private Set<String> emails = new HashSet<>();
 
 	@Bean
-	public ItemProcessor<Cliente, Cliente> procesadorValidacaoCustomProcessor() {
+	public ItemProcessor<Cliente, Cliente> procesadorValidacaoCustomProcessor() throws Exception {
+		return new CompositeItemProcessorBuilder<Cliente, Cliente>()
+				.delegates(beanValidatingProcessor(), emailValidatingProcessor()) // Lista todos os processadores que precisar aqui
+				.build();
+	}
 
-		// Não altera o dado, só valida e passa validado para o escritor
-		// Com validações mais customizadas
+
+
+	// Não altera o dado, só valida os dados obrigatórios e passa validado para o escritor
+	private BeanValidatingItemProcessor<Cliente> beanValidatingProcessor() throws Exception {
+		BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+		processor.setFilter(true); // Apenas filtra os itens inválidos mas não impede o processamento dos demais
+		processor.afterPropertiesSet();
+		return processor;
+	}
+
+
+	// Com validações mais customizadas
+	private ValidatingItemProcessor<Cliente> emailValidatingProcessor() {
 		ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
 		processor.setValidator(customValidator());
 		processor.setFilter(true); // Não interrompe o batch
 		return processor;
-
 	}
 
 	// Verificar se existe cliente duplicado
