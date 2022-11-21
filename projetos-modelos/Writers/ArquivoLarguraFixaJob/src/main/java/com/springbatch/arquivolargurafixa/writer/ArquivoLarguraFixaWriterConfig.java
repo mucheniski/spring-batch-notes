fb7a4e33520002c708c294436cd6fe0ebe047efa
mucheniski <mucheniski@gmail.com>
2022-11-21 12:37:15 -0300

    escritor arquivo flat formatado

--- projetos-modelos/Writers/ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/ArquivoLarguraFixaWriterConfig.java
+++ projetos-modelos/Writers/ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/ArquivoLarguraFixaWriterConfig.java
@@ -1,16 +1,34 @@
 package com.springbatch.arquivolargurafixa.writer;
 
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.springbatch.arquivolargurafixa.dominio.Cliente;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class ArquivoLarguraFixaWriterConfig {
-	// FIXME: Modificar para escrever em arquivo de largura fixa!
+
+	FileSystemResource arquivoSaida = new FileSystemResource("files/escrita.txt");
+
+
+	/*
+	Por padrão o preenchimento de espaços indicado com o percent % é feito na esquerda
+	para corrigirmos isso e preenchermos os espaços a direita colocamos o %-TamanhoColuna
+	 */
+
 	@Bean
-	public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
-		return items -> items.forEach(System.out::println);
+	public FlatFileItemWriter<Cliente> escritaArquivoLarguraFixaWriter() {
+		return new FlatFileItemWriterBuilder<Cliente>()
+				.name("escritaArquivoLarguraFixaWriter")
+				.resource(arquivoSaida)
+				.formatted() // Indica que é um arquivo formatado
+				.format("%-9s %-9s %-2s %-19s") // %-TamanhoColuna String - indica que a coluna tem tamanho fixo, completada com espaços se o texto nao atingir o tamanho informado
+				.names("nome", "sobrenome", "idade", "email") // Nomes das colunas do arquivo escrito
+				.build();
 	}
 }
